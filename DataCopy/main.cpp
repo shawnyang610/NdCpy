881e5cb87940c4a2897fe954ee987d81597706de
Shawn Yang <shawnyang610@gmail.com>
2018-06-17 23:30:34 -0400

    Initial Commit

---
+++ DataCopy/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  DataCopy
+//
+//  Created by Shawn Yang on 6/17/18.
+//  Copyright © 2018 Shawn Yang. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}

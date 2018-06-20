942b3a5a482c229e4525c604a2703648c6805a10
Shawn Yang <shawnyang610@gmail.com>
2018-06-19 21:53:51 -0400

    fixed getIOStartOffset logic

--- DataCopy/main.cpp
+++ DataCopy/main.cpp
@@ -23,14 +23,13 @@ bool hasOverlap(vector<size_t>&overlap_start, vector<size_t>overlap_end);
 size_t getPosition (vector<size_t>& start_pos, vector<size_t>& overlap_start);
 size_t getMinContDimn(vector<size_t>&input_start, vector<size_t>&input_count,
                               vector<size_t>&overlap_start, vector<size_t>overlap_count);
-void getRelativePos(vector<size_t>&rel_position, vector<size_t>&input_start,
-                               vector<size_t>&overlap_start);
-size_t getBlockSize(vector<size_t> overlap_count, size_t min_cont_dim, size_t element_size);
+size_t getBlockSize(vector<size_t> overlap_count, size_t min_cont_dim);
 void copy_cat(size_t lv, vector<char>&input,vector<char>&output,size_t& in_start_offset,
               size_t& out_start_offset,vector<size_t>in_elem_count,vector<size_t>out_elem_count,
               vector<size_t>&overlap_count,size_t min_cont_dim, size_t block_size, size_t elm_size);
 void getElmCount(vector<size_t>&io_elm_count,vector<size_t>&io_count);
-size_t getIOStartOffset(vector<size_t>&overlap_IO_rel_pos);
+size_t getIOStartOffset(vector<size_t>& io_start,vector<size_t>io_elm_counts,
+                        vector<size_t>& overlap_start);
 
 /*end of helper functions*/
 
@@ -58,14 +57,12 @@ int NdCopy(vector<char> input, vector<size_t> input_start, vector<size_t> input_
     getOverlapEnd(overlap_end, input_end, output_end);
     getOverlapCount(overlap_count,overlap_start, overlap_end);
     if (!hasOverlap(overlap_start, overlap_end)) return 1;//no overlap found
-    getRelativePos(overlap_input_rel_pos, input_start, overlap_start);
-    getRelativePos(overlap_output_rel_pos, output_start, overlap_start);
-    input_overlap_start_offset= getIOStartOffset(overlap_input_rel_pos);
-    output_overlap_start_offset=getIOStartOffset(overlap_output_rel_pos);
     getElmCount(input_elm_count,input_count);
     getElmCount(output_elm_count, output_count);
+    input_overlap_start_offset= getIOStartOffset(input_start,input_elm_count,overlap_start);
+    output_overlap_start_offset=getIOStartOffset(output_start,output_elm_count,overlap_start);
     min_cont_dim = getMinContDimn(input_start, input_count,overlap_start, overlap_count);
-    block_size= getBlockSize(overlap_count, min_cont_dim, element_size);
+    block_size= getBlockSize(overlap_count, min_cont_dim);
     copy_cat(0,input,output,input_overlap_start_offset, output_overlap_start_offset,
              input_elm_count, output_elm_count,overlap_count,min_cont_dim,block_size, element_size);
     //end of main algm
@@ -119,9 +116,10 @@ void getElmCount(vector<size_t>&io_elm_count,vector<size_t>&io_count){
         }
     }
 }
-size_t getIOStartOffset(vector<size_t>&overlap_IO_rel_pos){
+size_t getIOStartOffset(vector<size_t>& io_start,vector<size_t>io_elm_counts, vector<size_t>& overlap_start){
     size_t res=1;
-    for (size_t i:overlap_IO_rel_pos) res*=i;
+    for (size_t i=0; i<io_start.size();i++)
+        res=res+(overlap_start[i]-io_start[i])*io_elm_counts[i];
     return res;
 }
 size_t getMinContDimn(vector<size_t>&input_start, vector<size_t>&input_count,
@@ -137,8 +135,8 @@ size_t getMinContDimn(vector<size_t>&input_start, vector<size_t>&input_count,
     }
     return i;
 }
-size_t getBlockSize(vector<size_t> overlap_count, size_t min_cont_dim, size_t element_size){
-    size_t res=element_size;
+size_t getBlockSize(vector<size_t> overlap_count, size_t min_cont_dim){
+    size_t res=1;
     for (size_t i=min_cont_dim; i<overlap_count.size();i++)
         res*=overlap_count[i];
     return res;
@@ -149,63 +147,33 @@ size_t getBlockSize(vector<size_t> overlap_count, size_t min_cont_dim, size_t el
  W = overlap_area_count[0]*overlap_area_count[1]*...*overlap_area_count[total_dims-1]
  best case: B =1, happens when min_cont_dim is the top dimension
  */
-void copy_cat(size_t lv, vector<char>&input,vector<char>&output,size_t& in_start_offset,
+void copy_cat(size_t cur_dim, vector<char>&input,vector<char>&output,size_t& in_start_offset,
               size_t& out_start_offset,vector<size_t>in_elem_count,vector<size_t>out_elem_count,
               vector<size_t>&overlap_count,size_t min_cont_dim, size_t block_size, size_t elm_size){
     //note: all elements in and below this node is continuous on input
     //copy the continous data block
-    if (lv==min_cont_dim){
+    if (cur_dim==min_cont_dim){
         memcpy(&output+out_start_offset*elm_size, &input+in_start_offset*elm_size, block_size*elm_size);
         in_start_offset+=block_size;
         out_start_offset+=block_size;
     }
     //recursively call itself in order, for every element current node has
     //on a deeper level, stops upon reaching min_cont_dim
-    if (lv<min_cont_dim)
+    if (cur_dim<min_cont_dim)
         for (size_t i=0; i<overlap_count[i];i++)
-            copy_cat(lv+1, input, output, in_start_offset, out_start_offset,in_elem_count,
+            copy_cat(cur_dim+1, input, output, in_start_offset, out_start_offset,in_elem_count,
                      out_elem_count,overlap_count, min_cont_dim, block_size, elm_size);
     //the gap between current node and the next needs to be padded so that
     //next continous blocks starts at the correct position for both input and output
     //the size of the gap depends on the depth in dimensions,level backtracked and
     //the difference in element counts between the Input/output and overlap area.
-    in_start_offset+=in_elem_count[lv];
-    out_start_offset+=out_elem_count[lv];
+    in_start_offset+=in_elem_count[cur_dim];
+    out_start_offset+=out_elem_count[cur_dim];
 }
 /*end of helper function definitions*/
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-/*
- ====== TESTS ======
- */
-void test1(){
-    vector<char> src={'f','a','t','i','s','m','e','f','a','t','i','s','m','e',
-        'f','a','t','i','s','m','e','f','a','t','i','s','m','e','f','a','t','i','s','m','e','f','a','t','i','s','m','e'
-    };
-    vector<char>dest={'o','n','c','e','u','z'};
-    //std::memcpy(&dest, &src, 3);
-    //cout<<dest[0]<<dest[1]<<dest[2]<<dest[3]<<dest[4]<<dest[5]<<endl;
-    cout<<sizeof(src)<<endl;
-    cout<<sizeof(dest)<<endl;
-}
-
-
-
 int main(int argc, const char * argv[]) {
-    
-    test1();
     
     // insert code here...
     std::cout << "Hello, World!\n";
